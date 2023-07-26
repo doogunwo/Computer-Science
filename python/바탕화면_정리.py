dc03a473174c516a393c758b7df95ecd9e3fa8b6
doogunwo <dgw0601@naver.com>
2023-07-26 11:39:25 +0900

    slove 1

---
+++ python/바탕화면_정리.py
@@ -0,0 +1,22 @@
+def solution(wallpaper):
+    answer = []
+    y1 = []
+    x1 = []
+    for y in range(wallpaper):
+        
+        for x in range(len(wallpaper[y])):
+            
+            if wall[y][x] == "#":
+                
+                y1.append[y]
+                x1.append[x]
+    
+    
+    
+            
+
+
+    return answer
+
+wallpaper = ["..........", ".....#....", "......##..", "...##.....", "....#....."]
+answer = solution(wallpaper)
