8afd2bbd6cfdce4bb22310699ae7ba7d6069aa1c
doogunwo <dgw0601@naver.com>
2023-07-27 11:09:17 +0900

    c++ 삭제

--- .vscode/test.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>      //해더파일
-using namespace std;     //네임스페이스
- 
-int main(void)           //메인함수
-{
-    cout << "Hello World" << endl; //문자열 출력
-    
-    return 0; // 프로그램 끝
-}
