d4ea46a1b42a3c9f8e4db64a97cb9f0779be87ad
doogunwo <dgw0601@naver.com>
2023-07-30 23:45:14 +0900

    문제어렵네

---
+++ python/거스름돈.py
@@ -0,0 +1,17 @@
+def solution(n, money):
+    answer = 0
+
+    for i in range(len(money)):
+        point = money[i]
+        
+        
+
+
+    return answer/1000000007
+
+
+
+money = [1,2,5]
+n = 5
+res = solution(n,money)
+print(res)
---
+++ python/시험장나누기.py
@@ -0,0 +1,11 @@
+def solution(k, num, links):
+    answer = 0
+    
+    return answer
+
+
+k = 3
+num = [12, 30, 1, 8, 8, 6, 20, 7, 5, 10, 4, 1]
+links  = [[-1, -1], [-1, -1], [-1, -1], [-1, -1], [8, 5], [2, 10], [3, 0], [6, 1], [11, -1], [7, 4], [-1, -1], [-1, -1]]
+res = solution(k, num, links)
+print(res)
