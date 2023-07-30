eccea2a0c828420b6f7d818828e8d979fe0c4d04
doogunwo <dgw0601@naver.com>
2023-07-31 00:46:36 +0900

    커밋9

---
+++ python/합승택시요금.py
@@ -0,0 +1,18 @@
+def solution(n, s, a, b, fares):
+    answer = 0
+    taxi = []
+    return answer
+
+
+
+
+
+
+n=6 
+s=4
+a=6
+b=2
+fares = [[4, 1, 10], [3, 5, 24], [5, 6, 2], [3, 1, 41], [5, 1, 24], [4, 6, 50], [2, 4, 66], [2, 3, 22], [1, 6, 25]]
+
+result = solution(n,s,a,b,fares)
+print(result)
