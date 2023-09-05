449190f1e060d2d4eab281f3250b7560bdcbf487
도건우 <87505243+doogunwo@users.noreply.github.com>
2023-09-05 14:23:02 +0900

    같은숫자는싫어 풀엇음

--- python/타겟넘버.py
+++ python/타겟넘버.py
@@ -1,24 +1,6 @@
-answer = 0
-def dfs(idx, numbers, target, value):
-    global answer # 전역변수 사용한다는 선언
-
-    length = len(numbers)
-    # 전부 다 계산했을 때 target 과 같은 경우, 정답
-    if(idx == length and target == value):
-        answer += 1
-        return
-    # 전부 다 계산했을 때 target 과 다른 경우,
-    if(idx == length):
-        return 
-
-    # 현재 값을 더하거나 빼서 value 로 설정, 다음 index 계산을 위해 dfs 반복
-    dfs(idx+1, numbers, target, value+numbers[idx])
-    dfs(idx+1, numbers, target, value-numbers[idx])
-
-
 def solution(numbers, target):
     global answer # 전역변수 사용한다는 선언
     dfs(0, numbers, target, 0)
     return answer
 
-print(solution([1, 1, 1, 1, 1], 3))
+
