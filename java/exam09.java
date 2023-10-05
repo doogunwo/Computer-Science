f5cb5506e34525a8370151bc1de298d48b8d0bda
도건우 <87505243+doogunwo@users.noreply.github.com>
2023-10-05 15:38:07 +0900

    init

--- java/exam09.java
+++
@@ -1,8 +0,0 @@
-public class exam09 {
-    public static void main(String args[]) {
-        Automobile auto = new Automobile();
-
-        auto.upSpeed(250);
-        System.out.println(auto.getSpeed());
-    }
-}
