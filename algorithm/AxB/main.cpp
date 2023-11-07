a50596cdee951bfe02b141a0668abafb41e20ce8
도건우 <87505243+doogunwo@users.noreply.github.com>
2023-11-07 15:49:35 +0900

    1107 commit

---
+++ algorithm/AxB/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+
+    int* arr;
+    arr =new int[5];
+
+    for(int i = 0 ; i<5; i++){
+        cin >> arr[i];
+    }
+
+        
+
+}
