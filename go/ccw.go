47800b4c83b52aca37c779bd186f5712f01d668e
도건우 <87505243+doogunwo@users.noreply.github.com>
2024-01-11 00:27:51 +0900

    mmm

--- go/ccw.go
+++ go/ccw.go
@@ -1,8 +1,9 @@
 package main
 
+// 백준 11758번
 import "fmt"
 
-func ccw() int {
+func ccw_og() int {
 	var x1, y1, x2, y2, x3, y3 int
 	fmt.Scan(&x1, &y1, &x2, &y2, &x3, &y3)
 	result := (x2-x1)*(y3-y1) - (y2-y1)*(x3-x1)
