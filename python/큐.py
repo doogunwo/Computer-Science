cd41c6f74d0ca39c49fbfa503e57a5f4f21436ff
doogunwo <dgw0601@naver.com>
2024-01-31 18:13:39 +0900

    백준 10845번 파이썬

---
+++ python/큐.py
@@ -0,0 +1,24 @@
+from collections import deque
+import sys
+
+n = int(sys.stdin.readline())
+queue = deque()
+
+for _ in range(n):
+    command = sys.stdin.readline().split()
+
+    if command[0] == 'push':
+        queue.append(command[1])
+    elif command[0] == 'pop':
+        if queue:
+            print(queue.popleft())
+        else:
+            print(-1)
+    elif command[0] == 'size':
+        print(len(queue))
+    elif command[0] == 'empty':
+        print(0 if queue else 1)
+    elif command[0] == 'front':
+        print(queue[0] if queue else -1)
+    elif command[0] == 'back':
+        print(queue[-1] if queue else -1)
